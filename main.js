dabe1b7b9dee72aa315dd37c9d1605b135395f0c
darkenvy <darkenvy6@gmail.com>
2016-11-15 09:56:30 -0800

    event listeners not working on twitter, so going with bookmarklet method

--- main.js
+++ main.js
@@ -65,7 +65,6 @@ var ligatures = {
   "ft" :"ﬅ",
   "fs" :"ß",
   "fz" :"ß",
-  "et" :"&",
   "st" :"ﬆ",
   "db" :"ȸ",
   "dz" :"ʣ",
@@ -80,29 +79,24 @@ var ligatures = {
   "ll" :"ǁ",
   "nj" :"ǌ",
   "th" :"ᵺ",
-  "'n" :"ŉ",
+  // "'n" :"ŉ",
   "hu" :"ƕ",
-  "hv" :"ƕ",
-  "?!" :"‽",
-  "!?" :"‽"
+  "hv" :"ƕ"
+  // "?!" :"‽",
+  // "!?" :"‽"
 };
 
+var box1 = document.getElementById('tweet-box-home-timeline').children[0];
+var box2 = document.getElementById('tweet-box-global').children[0];
+var elem = box1;
 
-// Create and add button
-var btnHTML = document.createElement("div");
-btnHTML.innerHTML = "Slam It!";
-btnHTML.setAttribute('class', 'btn primary-btn slammer');
-document.getElementsByClassName('TweetBoxExtras tweet-box-extras')[1]
-        .appendChild(btnHTML);
+// Switch boxes from home-timeline to global
+if (box1.innerText == "What’s happening?" || box1.length <= 1 ) {elem = box2;}
 
-// Add event listener
-document.getElementsByClassName('slammer')[0].addEventListener('click', function(e) {
-  slammer();
-})
 
 // Add the function OFC
-var slammer = function() {
-  var curr = document.getElementById('tweet-box-global').children[0].innerText;
+var slammer = function(element) {
+  var curr = element.innerText;
   var result = '';
 
   // Go through each word first
@@ -121,49 +115,25 @@ var slammer = function() {
   result = result.replace(/(\w\.)(\w)/, function(m,p1,p2) {return p1 + p2.toUpperCase();}); // Uppercase new sentance
   result = result.replace(/^\w/, function(m) {return m.toUpperCase();}); // Cap first letter
 
-  document.getElementById('tweet-box-global').children[0].innerText = result;
+  element.innerText = result;
 }
 
-
-// // document.addEventListener("DOMContentLoaded", function() {
-// setTimeout(function() {
-//   // Add the button to the Tweet Popup
-//   var btnHTML = `<div id="slammer" class="btn primary-btn">Slam It</div>`;
-//   $('.TweetBoxExtras.tweet-box-extras').append(btnHTML);
-
-//   // Add event listener for the button we created
-//   $('#slammer').click(slammer);
-
-// // });
-
-//   function slammer() {
-//     var curr = $('#tweet-box-global')[0].children[0].innerText;
-//     var result = '';
-
-//     // Go through each word first
-//     curr.split(' ').forEach(function(item, idx) {
-//       result += item.replace(/(\w+)(\W+)?/i, function(match, p1, p2) {
-//         for (var key in ligatures) {p1 = p1.toLowerCase().replace(key, ligatures[key]);}
-//         return p2? p1 + p2: p1;
-//       });
-//       result += ' ';
-//     });
-
-//     // Then go through the whole tweet and free space through punctuation
-//     result = result.replace(/with\s/, 'w/'); // with me => w/me
-//     result = result.replace(/([,.?;:!])\s/g, '$1'); // Free space after punc.
-//     result = result.replace(/(?:'(\w))|(?:(\w)'\s)/g, '$1'); // remove ' in words
-//     result = result.replace(/(\w\.)(\w)/, function(m,p1,p2) {return p1 + p2.toUpperCase();}); // Uppercase new sentance
-//     result = result.replace(/^\w/, function(m) {return m.toUpperCase();}); // Cap first letter
-
-//     $('#tweet-box-global')[0].children[0].innerText = result;
-//   }
-
-
-
-// }, 5000);
+// execute
+slammer(elem);
 
 
 
 
+// human + hv = ?! lists craft florida avenue yet ai'nt llama moot 13ft them.
 
+// // Create and add button
+// var btnHTML = document.createElement("div");
+// btnHTML.innerHTML = "Slam It!";
+// btnHTML.setAttribute('class', 'btn primary-btn slammer');
+// document.getElementsByClassName('TweetBoxExtras tweet-box-extras')[1]
+//         .appendChild(btnHTML);
+
+// // Add event listener
+// document.getElementsByClassName('slammer')[0].addEventListener('click', function(e) {
+//   console.log('clicked');
+// })
